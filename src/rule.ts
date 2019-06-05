c5854146808fc70c991a8d3a07fe5cc9820492ec
mactkg <mactkg@gmail.com>
2019-06-05 19:35:20 +0900

    fix indent

--- src/rule.ts
+++ src/rule.ts
@@ -9,8 +9,8 @@ export function useMatcher(rules: Rule[]) : ((title : string, body : string, dif
   return (title : string, body : string, diff : string): string[] => {
     return rules.filter(rule => {
       return title.indexOf(rule.title) >= 0 || 
-      body.indexOf(rule.body) >= 0 ||
-      diff.indexOf(rule.diff) >= 0
+        body.indexOf(rule.body) >= 0 ||
+        diff.indexOf(rule.diff) >= 0
     }).map(rule => rule.channel)
   }
 }
