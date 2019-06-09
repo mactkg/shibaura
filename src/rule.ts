36f3cd29a84bb7d7af29ca24506f6d6508a1ca90
mactkg <mactkg@gmail.com>
2019-06-10 00:17:49 +0900

    Fix a bug that code matches every notification

--- src/rule.ts
+++ src/rule.ts
@@ -8,9 +8,9 @@ export interface Rule {
 export function useMatcher (rules: Rule[]) : ((title : string, body : string, diff : string) => string[]) {
   return (title : string, body : string, diff : string): string[] => {
     return rules.filter(rule => {
-      return title.indexOf(rule.title) >= 0 ||
-        body.indexOf(rule.body) >= 0 ||
-        diff.indexOf(rule.diff) >= 0
+      return rule.title != '' && title.indexOf(rule.title) >= 0 ||
+        rule.body != '' && body.indexOf(rule.body) >= 0 ||
+        rule.diff != '' && diff.indexOf(rule.diff) >= 0
     }).map(rule => rule.channel)
   }
 }
