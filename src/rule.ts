6b485f2f08a2778b78fe07d2c3b9f5616af42b95
mactkg <mactkg@gmail.com>
2019-06-13 15:37:40 +0900

    no debug

--- src/rule.ts
+++ src/rule.ts
@@ -10,7 +10,6 @@ const uniq = array => [...new Set<string>(array)];
 export function useMatcher (rules: Rule[]) : ((title : string, body : string, diff : string) => string[]) {
   return (title : string, body : string, diff : string): string[] => {
     return uniq(rules.filter(rule => {
-      console.log(rule, '\n', title.indexOf(rule.title), body.indexOf(rule.body), diff.indexOf(rule.diff), '\n')
       return rule.title != '' && title.indexOf(rule.title) >= 0 ||
         rule.body != '' && body.indexOf(rule.body) >= 0 ||
         rule.diff != '' && diff.indexOf(rule.diff) >= 0
